56ad9b713a2d3d2184050a0ce008eba7dc8e9022
ImNia <agnia-kimi1996@mail.ru>
2022-01-25 13:41:30 +0700

    Changed opening of the news by the link

--- app/src/main/java/com/delirium/reader/NewsReading.kt
+++ app/src/main/java/com/delirium/reader/NewsReading.kt
@@ -1,5 +1,7 @@
 package com.delirium.reader
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -26,16 +28,18 @@ class NewsReading : Fragment() {
         val binding = DataBindingUtil.inflate<NewsReadingBinding>(inflater,
             R.layout.news_reading, container, false)
 
-        runBlocking {
+        /*runBlocking {
             GlobalScope.launch {
                 text = getNews.getDescription(URL(arguments?.getString("link")))!!
             }.join()
-        }
+        }*/
 
-        binding.news = text?.let {
+        /*binding.news = text?.let {
             HtmlCompat.fromHtml(it, HtmlCompat.FROM_HTML_MODE_LEGACY).toString()
-        }
+        }*/
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(arguments?.getString("link")))
 
+        startActivity(intent)
         return binding.root
     }
 }
