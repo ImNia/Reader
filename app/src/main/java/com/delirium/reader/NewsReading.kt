bb63a850f52c0b9222c5a2ed7ca1dbec1be905d7
ImNia <agnia-kimi1996@mail.ru>
2022-01-25 16:24:47 +0700

    Opening links in application

--- app/src/main/java/com/delirium/reader/NewsReading.kt
+++ app/src/main/java/com/delirium/reader/NewsReading.kt
@@ -1,45 +1,29 @@
 package com.delirium.reader
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.text.HtmlCompat
-import androidx.databinding.DataBindingUtil
-import com.delirium.reader.databinding.NewsReadingBinding
-import com.delirium.reader.parser.GetNews
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import java.net.URL
+import android.webkit.WebView
 
 class NewsReading : Fragment() {
-    private val getNews = GetNews()
-    private var text: String? = null
+    private lateinit var webView: WebView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val binding = DataBindingUtil.inflate<NewsReadingBinding>(inflater,
-            R.layout.news_reading, container, false)
 
-        /*runBlocking {
-            GlobalScope.launch {
-                text = getNews.getDescription(URL(arguments?.getString("link")))!!
-            }.join()
-        }*/
+        val rootView = inflater.inflate(R.layout.news_reading, container, false)
 
-        /*binding.news = text?.let {
-            HtmlCompat.fromHtml(it, HtmlCompat.FROM_HTML_MODE_LEGACY).toString()
-        }*/
-        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(arguments?.getString("link")))
+        webView = rootView.findViewById(R.id.newsWebView)
+        webView.webViewClient = NewsWebViewClient()
+        webView.settings.javaScriptEnabled
 
-        startActivity(intent)
-        return binding.root
+        arguments?.getString("link")?.let{
+            webView.loadUrl(it)
+        }
+        return rootView
     }
 }
